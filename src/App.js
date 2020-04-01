ead54a52dba18b04a87477423578ea104fd2d06b
yanfeiwuji <1327800522@qq.com>
2020-04-01 12:22:52 +0800

    add user info

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css'
 import { Button } from 'antd'
 import req from './plugin/axios'
 import Users from './comp/users/Index'
+import Info from './comp/users/Info'
 
 const App = () => {
   return (
-    <div className='App'>
+    <div>
+      <Info />
       <Users />
     </div>
   )
---
+++ src/comp/users/Info.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Form, Input, Switch, Select } from 'antd'
+
+const Add = () => {
+  return (
+    <Form labelCol={{ span: 4 }} wrapperCol={{ span: 12 }} layout='horizontal'>
+      <Form.Item label='ID'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='创建时间'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='用户名'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='电子邮箱'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='姓'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='名'>
+        <Input />
+      </Form.Item>
+      <Form.Item label='是否可用'>
+        <Switch />
+      </Form.Item>
+
+      <Form.Item label='验证电子邮箱'>
+        <Switch />
+      </Form.Item>
+      <Form.Item label='用户需要操作'>
+        <Select>
+          <Select.Option value='1'>1</Select.Option>
+          <Select.Option value='2'>2</Select.Option>
+          <Select.Option value='3'>3</Select.Option>
+        </Select>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default Add
